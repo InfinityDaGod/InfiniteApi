9ca3981690da478fa85b9dd36571c4f33d21bdb1
Inf1n1ty <78967618+InfinityDaGod@users.noreply.github.com>
2021-10-07 16:05:45 +0000

    stuff

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+// ------ PACKAGE IMPORTS ------ //
+
 const express = require('express')
 const app = express()
 const rateLimit = require ("express-rate-limit")
 const port = 3000;
+
+// ------ RATE LIMITER ------ //
 
 const limiter = rateLimit({
     windowMs: 100 * 60 * 100, // 10 minutes
     max: 100, // 100 requests max every 10 minutes
     message:"You are sending too many requests. Please try again later."
 })
+
+// ------ ROUTES HANDLER ------ //
 
 require("./routes")(app)
 
